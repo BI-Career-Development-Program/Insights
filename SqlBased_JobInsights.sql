567a16355aed8fcc09fbf72554fdb8ab47a2de32
simofleanta <simo.fleanta@gmail.com>
2021-01-22 13:11:54 +0100

    Update SqlBased_JobInsights.sql

--- SqlBased_JobInsights.sql
+++ SqlBased_JobInsights.sql
@@ -4,9 +4,14 @@ select Type_ownership, Sector, Rating, Size, Job_title, (select Max(Salary_Estim
 WHERE  Job_title='Business Analyst'
 Limit 200;
 
--- Min salary from Jobs
+-- Min  BA job salary from Jobs
 select Type_ownership, Sector, Rating, Size, Job_title, (select Max(Salary_Estimate_k) from ba) from ba
 WHERE  Job_title='Business Analyst'
 Limit 200;
 
+-- Avg salary estimate by sector as BA
+select Sector, AVG(Salary_Estimate_k) FROM BA WHERE Job_title='Business Analyst'
+GROUP BY Sector;
+
+-- The relationship between comapny rating and salary estimate will be illustrated on Python.
 
